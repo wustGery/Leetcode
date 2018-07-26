ad790d6b6361c5258106f7fcb4e3b254c48eeaeb
wustGery <2458314507@qq.com>
2018-07-26 21:05:59 +0800

    26.cpp

---
+++ 26.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        if(nums.size()==0)  return 0;
+        vector<int>::iterator iter = nums.begin();
+        int pre_value = *iter;
+        ++iter;
+        for(;iter!=nums.end();)
+        {
+            int value = *iter;
+            if(value==pre_value)
+                nums.erase(iter);
+            else 
+            {
+                pre_value = value;
+                ++iter;
+            }
+        }
+        return nums.size();
+    }
+};
