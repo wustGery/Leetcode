8086be03ca2f5bf83d8b6cf645696a9f238ab324
wustGery <2458314507@qq.com>
2018-07-27 16:12:47 +0800

    28.cpp

---
+++ 28.cpp
@@ -0,0 +1,44 @@
+class Solution 
+{
+public:
+    void getNext(string p,int *next)
+    {
+        int p_len = p.length();
+        int j,k;
+        next[0] = -1;
+        j = 0;
+        k = -1;
+        while(j<p_len-1)
+        {
+            if(k==-1||p[k]==p[j])  next[++j] = ++k;
+            else k = next[k];
+        }
+    }
+
+    int KMP(string s,string p,int *next)
+    {
+        int i,j;
+        i = j = 0;
+        int s_len = s.length();
+        int p_len = p.length();
+        while(i<s_len&&j<p_len)
+        {
+            if(j==-1||s[i]==p[j])
+            {
+                ++i;
+                ++j;
+            }
+            else j = next[j];
+        }
+        return j==p_len?i-j:-1;
+    }
+    
+    int strStr(string haystack, string needle) 
+    {
+        int needle_len = needle.length();
+        int *next = new int[needle_len];
+        getNext(needle,next);
+        int ans = KMP(haystack,needle,next);
+        return ans;
+    }
+};
